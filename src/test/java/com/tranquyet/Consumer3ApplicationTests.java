5aac8694de6b4e526f63dfbd66f71ed720308c97
Trần Quyết <62180319+kaiTran56@users.noreply.github.com>
2022-01-06 00:15:25 +0700

    test

--- src/test/java/com/tranquyet/Consumer3ApplicationTests.java
+++ src/test/java/com/tranquyet/Consumer3ApplicationTests.java
@@ -11,3 +11,36 @@ class Consumer3ApplicationTests {
     }
 
 }
+
+@SpringBootTest(classes= WorldMeBlogApplication.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class TestMock {
+    @Autowired
+    private AssetManager assetManager;
+    @Test
+    @Order(1)
+    @Sql(scripts = {"test_post_before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    public void initData(){
+        System.out.println("Init data");
+        System.out.println("-----------------------------------------------------> INIT SIZE: "+assetManager.getLoves().size());
+    }
+    @Order(2)
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
+    @ParameterizedTest
+    @CsvSource({
+            "1,1"
+    })
+    public void test_postConstruct(int numb_1, int numb_2){
+        System.out.println("-----------------------------------------------------> AFTER SIZE: "+assetManager.getLoves().size());
+        assertEquals(numb_1,numb_2);
+    }
+    @Order(3)
+    @ParameterizedTest
+    @CsvSource({
+            "1,1"
+    })
+    @Sql(scripts = "test_after.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void destroyData(int numb_1, int numb_2){
+        System.out.println("Destroy data"); assertEquals(numb_1,numb_2);
+    }
+}
